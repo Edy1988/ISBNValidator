b2edb2fbc599eb391a3aa8b523bc1a161db23988
Edyta Bednarz <edy.b.bednarz@gmail.com>
2019-07-09 15:36:40 +0100

    Changed type if ISBN to string from int to allow 0 as a first digit.

--- test/learningJava/ValidateISBNTest.java
+++ test/learningJava/ValidateISBNTest.java
@@ -10,14 +10,20 @@ public class ValidateISBNTest {
     @Test
     public void checkAValidISBNfor10digitNumber(){
         ValidateISBN validator = new ValidateISBN();
-        boolean result = validator.checkISBN(140449116);
-        assertTrue(result);
-  }
+        boolean result = validator.checkISBN("0140449116");
+        assertTrue(result, "First value ");
+
+        result = validator.checkISBN("0140449116");
+        assertTrue(result, "Second value");
+
+    }
 
     @Test
     public void checkAnInValidISBNfor10digitNumber(){
         ValidateISBN validator = new ValidateISBN();
-        boolean result = validator.checkISBN(140449117);
+
+        boolean result = validator.checkISBN("0140449117");
+
         assertFalse(result);
     }
 }
