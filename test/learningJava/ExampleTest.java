a8411d2d0678fca8b012751cc3f78acc1a8eb968
Edyta Bednarz <edy.b.bednarz@gmail.com>
2019-07-09 12:47:25 +0100

    Project setup with gitignore and empty class

---
+++ test/learningJava/ExampleTest.java
@@ -0,0 +1,13 @@
+package learningJava;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class ExampleTest {
+    @Test
+
+    public void exampleTest(){
+        fail();
+    }
+}
